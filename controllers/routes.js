cc3a62d9545236185e398c74491a9dd2bf2c8e23
(20115904) Aravh Krishna Prasanth <20115904@setu.ie>
2026-02-05 16:37:47 +0000

    dd

---
+++ controllers/routes.js
@@ -0,0 +1,18 @@
+'use strict';
+import logger from "../utils/logger.js";
+import express from 'express';
+const router = express.Router();
+
+import start from './start.js';
+import dashboard from './dashboard.js';
+import about from './about.js';
+
+router.get('/', start.createView);
+router.get('/dashboard', dashboard.createView);
+router.get('/about', about.createView);
+
+router.get('/error', (request, response) =>
+  response.status(404).end('Page not found.')
+);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+import logger from "./utils/logger.js";
+
+import express from 'express';
+import routes from './controllers/routes.js';
+
+const app = express();
+const port = 3000;
+
+app.use('/', routes);
+
+app.listen(port, () => console.log(`Express app running on port ${port}!`));
+
